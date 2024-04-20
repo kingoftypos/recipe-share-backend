333743aba8f2533849fab4c8b364657404ca018e
Nishita Pande <89330962+nishitapande@users.noreply.github.com>
2024-04-20 09:52:23 +0530

    added files

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const env = require("dotenv");
+const connectDb = require("./config/dbConfig");
 
+env.config();
+
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
