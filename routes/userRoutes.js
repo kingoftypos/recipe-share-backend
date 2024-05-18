ae50c99dbda82904a6ca898219c7ef5bd81ec209
Shreyansh mishra <mshreyansh121@gmail.com>
2024-05-18 05:57:12 +0530

    added cors middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const router = express.Router();
 const userMiddleware = require("../middlewares/userMiddleware");
+const corsMiddelware=require("../middlewares/corsMiddleware");
 
 router.use(cors());
 
-router.post("/register", userMiddleware.createUser);
-router.post("/login",userMiddleware.loginUser);
-router.post("/forget-password",userMiddleware.forgetPassword);
-router.post("/reset-password/:id/:token",userMiddleware.resetPassword);
+router.post("/register", corsMiddelware,userMiddleware.createUser);
+router.post("/login",corsMiddelware,userMiddleware.loginUser);
+router.post("/forget-password",corsMiddelware,userMiddleware.forgetPassword);
+router.post("/reset-password/:id/:token",corsMiddelware,userMiddleware.resetPassword);
 
 module.exports = router;
