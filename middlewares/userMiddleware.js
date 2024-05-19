de375c76b57dec71c68ac07264c5bb866489bfa8
Nishita Pande <89330962+nishitapande@users.noreply.github.com>
2024-05-19 12:08:51 +0530

    Update userMiddleware.js

--- middlewares/userMiddleware.js
+++ middlewares/userMiddleware.js
@@ -54,7 +54,7 @@ exports.loginUser = async (req, res, next) => {
       expiresIn: process.env.JWT_EXPRIRESIN,
     });
     res.cookie("token", token, {
-      maxAge: process.env.JWT_EXPRIRESIN,
+      maxAge: 604800000,
       httpOnly: true,
     });
     res.status(200).json({
