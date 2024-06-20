af639c507e5e491fe5db13f66bd95a94fd13a9f5
Nishita Pande <89330962+nishitapande@users.noreply.github.com>
2024-06-20 12:51:38 +0530

    Update corsMiddleware.js

--- middlewares/corsMiddleware.js
+++ middlewares/corsMiddleware.js
@@ -2,13 +2,12 @@ module.exports = function (req, res, next) {
   res.setHeader(
     "Access-Control-Allow-Origin",
     "https://recipe-share-frontend.vercel.app"
-    // "http://localhost:3000"
-  ); // Replace with your frontend URL
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, DELETE, OPTIONS"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true"); // If you're allowing credentials
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 };
